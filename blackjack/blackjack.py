9ebf3c36394ce0f86147467973cc7991ef209c00
ykskks <fkt.kohei@gmail.com>
2021-09-02 22:24:39 +0900

    [feat] add Card

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,5 +1,26 @@
+class Card:
+    def __init__(self, suit: str, n: int):
+        self._suit = suit
+        self._n = n
+
+    @property
+    def point(self) -> int:
+        """このカードのポイントを取得する。
+
+        Returns:
+            int: カードのポイント
+        """
+        return min(self._n, 10)
+
+
 class Deck:
-    pass
+    def __init__(self):
+        self.cards = [
+            Card(suit, n) for suit in ["H", "D", "C", "S"] for n in range(1, 14)
+        ]
+
+    def shuffle(self) -> None:
+        pass
 
 
 class Player:
